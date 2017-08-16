b5b125ff23b1b19029a4dab6adb50cdd10c9a651
yangchuan <1107410196@qq.com>
2017-08-17 01:10:43 +0800

    add ckeditor

--- jiujiuchuan/settings.py
+++ jiujiuchuan/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'users',
     'index',
     'blog',
@@ -146,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
